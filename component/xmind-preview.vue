95d3869f91ec788be454a553045ba63d207c1163
codezengjie <codezengjie@gmail.com>
2024-04-21 17:06:24 +0800

    add: xmind预览

---
+++ component/xmind-preview.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="xmind">右键拖拽文件位置</div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+const props = defineProps({
+  xmindFilePath: {
+    type: String,
+    default: '',
+    required: true
+  }
+});
+
+onMounted(async () => {
+  const script = document.createElement('script');
+  script.src = 'https://unpkg.com/xmind-embed-viewer/dist/umd/xmind-embed-viewer.js';
+  script.onload = async () => {
+    const XMindEmbedViewer = window.XMindEmbedViewer;
+    const viewer = new XMindEmbedViewer({
+      el: '#xmind',
+      region: 'cn'
+    });
+    fetch(props.xmindFilePath)
+      .then(res => res.arrayBuffer())
+      .then(file => viewer.load(file));
+  };
+  document.head.appendChild(script);
+});
+</script>
+
+<style scoped>
+</style>
